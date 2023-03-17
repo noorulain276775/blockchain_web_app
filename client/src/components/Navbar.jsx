6035295067b8948ad6dd70ec9130a66c873beb85
Noor ul ain Ibrahim <noorulainibrahim75@gmail.com>
2023-03-17 14:24:42 +0400

    navbar is in process

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+import Logo from "../../images/logo.png"
+
+const Navbar = () => {
+  return (
+    <nav className="w-full flex md:justify-center justify-between items-center p-4">
+    <div className="md:flex">
+    </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <div>
+    <h1>I am welcome</h1>
+    </div>
+  )
+}
+
+export default Welcome
